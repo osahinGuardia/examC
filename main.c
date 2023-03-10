10d2daf58eb302aa5c70b73edb9db7681836db8b
Juste_ozan <117074595+osahinGuardia@users.noreply.github.com>
2023-03-10 09:56:57 +0100

    Ajout de la fonction saisir

--- main.c
+++ main.c
@@ -8,6 +8,10 @@
 // de solution dans tableauJoueur afin d'initialiser une grille jouable ( car solution est bel est bien la solution, donc pas jouable par définition. ).
 // un parametre doit permettre de verifier la condition de victoire en un seul coup (proposer votre façon de faire :) ) 
 
+
+
+
+
 // 2- une fonction saisir qui demande au joueur de saisir 3 informations: i, j , et v qui permettront de placer la valeur v aux coordonnées (i,j)
 
 // 3- apres chaque coordonnées il faut verifier les regles du sudoku sur le tableauJoueur donc : 
@@ -16,6 +20,69 @@
     // - la fonction verifierGrille (qui fait ceci pour toute la grille et permet de definir la condition de victoire)
 
 // 4 - Ecrire le code principal qui permet de jouer au joueur :  le programme prend fin quand on gagne 
+
+
+
+
+
+
+
+
+
+
+
+
+
+int saisir(int tab[8][8]){
+    int result = 0;
+    
+    while(result != 1){
+        int saisie = 0;
+        
+        int tabTemp[8][8] = {
+            {5,5,5,5, 5,5,5,5},
+            {5,5,5,5, 5,5,5,5},
+            {5,5,5,5, 5,5,5,5},
+            {5,5,5,5, 5,5,5,5},
+            
+            {5,5,5,5, 5,5,5,5},
+            {5,5,5,5, 5,5,5,5},
+            {5,5,5,5, 5,5,5,5},
+            {5,5,5,5, 5,5,5,5},
+            
+        };
+        
+        copyArray(tab, tabTemp);
+        
+        printf("Veuillez saisir un I");
+        scanf("%d", &saisie);
+        int saisieI = saisie;
+        
+        printf("Veuillez saisir un J");
+        scanf("%d", &saisie);
+        int saisieJ = saisie;
+        
+        if(tab[saisieX][saisieY] == 5){
+            printf("Cette case est deja remplie Reessayez\n");
+        } else {
+            printf("Quelle valeur souhaitez vous inserer ?\n");
+            scanf("%d", &saisie);
+            int saisieVal = saisie;
+            tabTemp[saisieX][saisieY] = saisieVal;
+            result = verifierGrille(tabeauTemporaire);
+            if(result){
+                tab[saisieX][saisieY] = saisieVal;
+            }
+        }
+        
+    }
+    
+    
+}
+
+
+
+
 
 int main(){
     // Ne pas toucher le code entre les commentaires
